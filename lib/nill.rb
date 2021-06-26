b135c0f8c77ad287c59c1787f77e0e92389220b9
ShintaKanayama <shinta327tuba@gmail.com>
2021-06-26 07:35:19 +0000

    Regexp

--- lib/nill.rb
+++ lib/nill.rb
@@ -1,4 +1,12 @@
-names = ['田中', '鈴木', '佐藤']
-san_names = names.map{ |name| "#{name}さん"}.join('と')
+html = <<-HTML
+<select name="game_console">
+<option value="none"></option>
+<option value="wii_u" selected>Wii U</option>
+<option value="ps4">プレステ4</option>
+<option value="gb">ゲームボーイ</option>
+</select>
+HTML
 
-p san_names
+replaced = html.gsub(/<option value="(\w+)"(?: selected)?>(.*)<\/option>/, '\1,\2')
+
+puts replaced
